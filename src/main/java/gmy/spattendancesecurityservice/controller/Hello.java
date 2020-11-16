498ff9fa362e36c0f5cabcd0351da97e99e33b56
GMY-Z <1321879427@qq.com>
2020-11-16 15:54:16 +0800

    hahtest

--- src/main/java/gmy/spattendancesecurityservice/controller/Hello.java
+++ src/main/java/gmy/spattendancesecurityservice/controller/Hello.java
@@ -1,19 +1,23 @@
 package gmy.spattendancesecurityservice.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @authon GMY
  * @create 2020-11-16 15:26
  */
 
-@Controller
+@RestController
+@EnableAutoConfiguration
 public class Hello {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "daf";
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
     }
 
 }
