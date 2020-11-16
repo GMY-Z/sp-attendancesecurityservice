38095c1de485d3e967453e32988368dcced6eb73
Guanmuyang <1321879427@qq.com>
2020-11-16 15:07:56 +0800

    first

---
+++ src/main/java/gmy/spattendancesecurityservice/SpAttendancesecurityserviceApplication.java
@@ -0,0 +1,13 @@
+package gmy.spattendancesecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAttendancesecurityserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAttendancesecurityserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gmy/spattendancesecurityservice/SpAttendancesecurityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package gmy.spattendancesecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpAttendancesecurityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
