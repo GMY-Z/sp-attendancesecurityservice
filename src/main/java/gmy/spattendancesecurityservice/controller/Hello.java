149dbdc6ae9fd387f3353e7e47db1bfefc67f248
Guanmuyang <1321879427@qq.com>
2020-11-16 15:31:21 +0800

    test

---
+++ src/main/java/gmy/spattendancesecurityservice/controller/Hello.java
@@ -0,0 +1,19 @@
+package gmy.spattendancesecurityservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @authon GMY
+ * @create 2020-11-16 15:26
+ */
+
+@Controller
+public class Hello {
+
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println(1);
+    }
+
+}
